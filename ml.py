5f777b93a96b223b601adf968fe373a72beb3dc0
Hansespinosa2 <andresespinosa@ufl.edu>
2024-11-19 21:36:55 -0500

    Add ml.py and activation functions

---
+++ ml.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+class ActivationFunctions():
+    def __init__():
+        pass
+
+    def relu(x):
+        return np.maximum(x, 0)
+    
+    def relu_cubed(x):
+        return np.power(np.maximum(x,0),3)
+    
+    
