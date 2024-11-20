9e718a6fe1542bc43e6c4495fc107390ec8aafdc
Hansespinosa2 <andresespinosa@ufl.edu>
2024-11-19 22:02:16 -0500

    Start but not finish the HiddenLayer Class

--- ml.py
+++ ml.py
@@ -1,15 +1,21 @@
 import numpy as np
 
 class ActivationFunctions():
-    def __init__():
+    def __init__(self):
         pass
 
-    def relu(x):
+    def relu(self, x):
         return np.maximum(x, 0)
     
-    def relu_cubed(x):
+    def relu_cubed(self, x):
         return np.power(np.maximum(x,0),3)
     
-    def linear(x):
+    def linear(self, x):
         return x
     
+class HiddenLayer():
+    def __init__(self, input_size, output_size):
+        self.input_size = input_size
+        self.output_size = output_size
+        W = np.random.rand(self.input_size, self.output_size)
+        b = np.random.rand(self.input_size, 1)
