de9ccc3ba1208ef2a78c486f6d1c05c3195b6118
Hansespinosa2 <andresespinosa@ufl.edu>
2024-11-19 23:52:09 -0500

    Add unfinished InputLayer class

--- ml.py
+++ ml.py
@@ -13,9 +13,21 @@ class ActivationFunctions():
     def linear(self, x):
         return x
     
+
+class InputLayer():
+    def __init__(self,input_size, output_size):
+        self.output_size = output_size
+        self.W = np.random.rand(self.output_size, input_size)
+
+    def forward(self, X):
+        return self.W.T @ X
+
 class HiddenLayer():
     def __init__(self, input_size, output_size):
         self.input_size = input_size
         self.output_size = output_size
-        W = np.random.rand(self.input_size, self.output_size)
-        b = np.random.rand(self.input_size, 1)
+        self.W = np.random.rand(self.output_size, self.input_size)
+        self.b = np.random.rand(self.output_size, 1)
+
+    def forward(self, X):
+        return self.W.T @ X + self.b
